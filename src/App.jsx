8dd00665f31bef3b6e0cc1000e912e16f67b0023
evelynnsg-collab <evelyn.nsg@gmail.com>
2026-06-24 11:12:12 -0300

    Add TURN servers for mobile network support

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,17 @@ function RadioTab({ userName }) {
 
   // Cria peer connection para um usuário remoto
   const createPeer = (peerId, isInitiator) => {
-    const pc = new RTCPeerConnection({ iceServers:[{urls:"stun:stun.l.google.com:19302"},{urls:"stun:stun1.l.google.com:19302"}] });
+    const pc = new RTCPeerConnection({ 
+    iceServers:[
+      {urls:"stun:stun.l.google.com:19302"},
+      {urls:"stun:stun1.l.google.com:19302"},
+      {urls:"stun:stun2.l.google.com:19302"},
+      {urls:"stun:stun3.l.google.com:19302"},
+      {urls:"turn:openrelay.metered.ca:80", username:"openrelayproject", credential:"openrelayproject"},
+      {urls:"turn:openrelay.metered.ca:443", username:"openrelayproject", credential:"openrelayproject"},
+      {urls:"turn:openrelay.metered.ca:443?transport=tcp", username:"openrelayproject", credential:"openrelayproject"},
+    ]
+  });
     peers.current[peerId] = pc;
 
     pc.onicecandidate = (e) => {
